1b30e0012d860ac4aee0cbb96a9a4852ac8dd0e7
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-08-14 00:51:30 +0700

    trying to use web components to make navbar and footer

---
+++ src/components/footer.ts
@@ -0,0 +1,59 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super()
+        const title = 'Ini Footernya Make Web Component hehe'
+        const desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+        const links = [
+            {icon: '<i class="fa-brands fa-facebook-f"></i>', to: 'fruits.html'},
+            {icon: '<i class="fa-brands fa-instagram"></i>', to: 'vegg.html'},
+            {icon: '<i class="fa-brands fa-twitter"></i>', to: 'others.html'},
+        ] 
+        const shadowRoot = this.attachShadow({mode: 'open'})
+        
+        const content = `
+            <style>
+                * {
+                box-sizing: border-box;
+                }
+                footer {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    gap: 10px;
+                    padding-inline: 30px;
+                    max-width: 1240px;
+                    text-align: center;
+                    background-color: #c4c4c4;
+                }
+                .title {
+                    color: #c4c4c4;    
+                }
+                .links-container{
+                    display: flex;
+                    gap: 20px;
+                }
+                a {
+                    text-decoration: none;
+                    padding: 20px;
+                    border-radius: 50%;
+                    color: white;
+                    background-color: #ebebeb;
+                }
+            </style>
+            <footer>
+                <h1 class='title'>${title}</h1>
+                <p>${desc}</p>
+                <div class='links-container'>
+                    ${
+                        links.map(link => (
+                            `<a href=${link.to}>${link.icon}</a>`
+                        )).join('')
+                    }
+                </div>
+            </footer>
+        `
+        shadowRoot.innerHTML = content
+    }
+}
+
+customElements.define('my-footer', Footer)
---
+++ src/components/navbar.ts
@@ -0,0 +1,62 @@
+class Navbar extends HTMLElement {
+    constructor() {
+        super()
+        const title = 'LOGO'
+        const links = [
+            {name: 'Fruits', to: 'fruits.html'},
+            {name: 'Veggetables', to: 'vegg.html'},
+            {name: 'Others', to: 'others.html'},
+        ] 
+        const shadowRoot = this.attachShadow({mode: 'open'})
+        
+        const content = `
+            <style>
+                * {
+                    box-sizing: border-box;
+                }
+                nav {
+                    width: 100%;
+                    padding-left:20px;
+                    padding-right:20px;
+                }
+                .nav-content-container{
+                    max-width: 1240px;
+                    margin-left:auto;
+                    margin-right:auto;
+
+                    display: flex;
+                    align-items: center;
+                    gap: 30px;
+                }
+                .links-container{
+                    display: flex;
+                    gap: 20px;
+                }
+                a {
+                    text-decoration: none;
+                    padding-top: 10px;
+                    padding-bottom: 10px;
+                    padding-inline: 30px;
+                    border-radius: 15px;
+                    color: #878787;
+                    background-color: #ebebeb;
+                }
+            </style>
+            <nav>
+                <div class='nav-content-container'>
+                    <h1>${title}</h1>
+                    <div class='links-container'>
+                        ${
+                            links.map(link => (
+                                `<a href=${link.to}>${link.name}</a>`
+                            )).join('')
+                        }
+                    </div>
+                </div>
+            </nav>
+        `
+        shadowRoot.innerHTML = content
+    }
+}
+
+customElements.define('my-navbar', Navbar)
